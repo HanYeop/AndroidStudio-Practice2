7f3ea31cab3cf266262da2ae931353c4a23ff8a3
Hanyeop <super7615@naver.com>
2021-05-21 23:39:12 +0900

    LottieEx 완료

--- LottieEx/app/src/main/java/org/techtown/lottieex/MainActivity.kt
+++ LottieEx/app/src/main/java/org/techtown/lottieex/MainActivity.kt
@@ -2,10 +2,19 @@ package org.techtown.lottieex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.techtown.lottieex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.animationView.setOnClickListener {
+            binding.animationView.playAnimation()
+
+        }
     }
 }
