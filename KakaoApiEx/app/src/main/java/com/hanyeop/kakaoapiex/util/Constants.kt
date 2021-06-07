af2aaa8c381ac21ca38bc3571ae94e6560acd518
Hanyeop <super7615@naver.com>
2021-06-07 12:49:44 +0900

    KakaoAPiEx 완료, api키 hide

--- KakaoApiEx/app/src/main/java/com/hanyeop/kakaoapiex/util/Constants.kt
+++ KakaoApiEx/app/src/main/java/com/hanyeop/kakaoapiex/util/Constants.kt
@@ -1,14 +1,14 @@
 package com.hanyeop.kakaoapiex.util
 
-import androidx.viewbinding.BuildConfig
+import com.hanyeop.kakaoapiex.util.ApiKey.Companion.REST_API_KEY
 
 // 상수 저장
 class Constants {
 
     companion object{
         const val BASE_URL = "https://dapi.kakao.com"
-        // 개인 API 사용
 
-        const val AUTH_HEADER = "KakaoAK $"
+        // 개인 API 사용
+        const val AUTH_HEADER = "KakaoAK $REST_API_KEY"
     }
 }
