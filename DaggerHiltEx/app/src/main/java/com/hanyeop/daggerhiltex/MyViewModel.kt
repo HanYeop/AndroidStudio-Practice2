d91c6a0a93dc27483a87c997463676c8edb3323c
Hanyeop <super7615@naver.com>
2021-06-12 15:36:57 +0900

    DaggerHilt ViewModel 주입 방식 수정 (2.31)

--- DaggerHiltEx/app/src/main/java/com/hanyeop/daggerhiltex/MyViewModel.kt
+++ DaggerHiltEx/app/src/main/java/com/hanyeop/daggerhiltex/MyViewModel.kt
@@ -1,11 +1,13 @@
 package com.hanyeop.daggerhiltex
 
 import android.util.Log
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import javax.inject.Named
 
-class MyViewModel @ViewModelInject constructor(
+@HiltViewModel
+class MyViewModel @Inject constructor(
     @Named("String2") string2 : String
 ) : ViewModel(){
 
