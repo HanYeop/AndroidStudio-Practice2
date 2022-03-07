fff3d2dd286ad65f9ac346c5df3fc4d68de36339
super7615 <super7615@naver.com>
2022-03-07 22:42:36 +0900

    FlowEx 완료

--- FlowEx/app/src/main/java/com/hanyeop/flowex/MainActivity.kt
+++ FlowEx/app/src/main/java/com/hanyeop/flowex/MainActivity.kt
@@ -2,10 +2,46 @@ package com.hanyeop.flowex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import com.hanyeop.flowex.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val myViewModel : MyViewModel by viewModels()
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        // 데이터바인딩
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        // 뷰모델 연결
+        binding.vm = myViewModel
+
+        // 뷰모델을 LifeCycle 에 종속시킴, LifeCycle 동안 옵저버 역할을 함
+        binding.lifecycleOwner = this
+
+//        // 1. LiveData
+//        myViewModel.liveData.observe(this){
+//            binding.textView.text = it.toString()
+//        }
+//
+//        // 2. StateFlow
+//        lifecycleScope.launchWhenStarted {
+//            myViewModel.stateFlow.collectLatest {
+//                binding.textView2.text = it.toString()
+//            }
+//        }
+
+        // 3. SharedFlow
+        lifecycleScope.launchWhenStarted {
+            myViewModel.sharedFlow.collectLatest {
+                Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
---
+++ FlowEx/app/src/main/java/com/hanyeop/flowex/MyViewModel.kt
@@ -0,0 +1,44 @@
+package com.hanyeop.flowex
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MyViewModel : ViewModel() {
+    private val _liveData = MutableLiveData(100)
+    val liveData: LiveData<Int> = _liveData
+
+    private val _stateFlow = MutableStateFlow(200)
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    fun changeLiveData(){
+        viewModelScope.launch {
+            repeat(10){
+                _liveData.value = _liveData.value?.plus(1)
+                delay(1000L)
+            }
+        }
+    }
+
+    fun changeStateFlow(){
+        viewModelScope.launch {
+            repeat(10){
+                _stateFlow.value = _stateFlow.value.plus(1)
+                delay(1000L)
+            }
+        }
+    }
+
+    fun changeSharedFlow(){
+        viewModelScope.launch {
+            _sharedFlow.emit("안녕하세요")
+        }
+    }
+}
