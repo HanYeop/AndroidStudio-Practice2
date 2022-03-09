38abca3d948641971ad94b53c7e57d5526435ba9
super7615 <super7615@naver.com>
2022-03-09 23:41:09 +0900

    BaseEx - 1. 기존 방식의 DataBinding

--- BaseEx/app/src/main/java/com/hanyeop/baseex/MainActivity.kt
+++ BaseEx/app/src/main/java/com/hanyeop/baseex/MainActivity.kt
@@ -2,10 +2,16 @@ package com.hanyeop.baseex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.hanyeop.baseex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
     }
 }
