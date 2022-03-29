a8fdecdccfc9e02778a7da75da235a5518812dd7
super7615 <super7615@naver.com>
2022-03-29 14:52:51 +0900

    ActivityResultLauncherEx - 2. Permission

--- ActivityResultLauncher/app/src/main/java/com/hanyeop/activityresultlauncherex/MainActivity.kt
+++ ActivityResultLauncher/app/src/main/java/com/hanyeop/activityresultlauncherex/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.hanyeop.activityresultlauncherex
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.hanyeop.activityresultlauncherex.Constant.Companion.ONE
 import com.hanyeop.activityresultlauncherex.Constant.Companion.TWO
 import com.hanyeop.activityresultlauncherex.databinding.ActivityMainBinding
@@ -33,6 +36,23 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity,SubTwoActivity::class.java)
                 activityResultLauncher.launch(intent)
             }
+
+            // 카메라 권한 요청 - 1
+            perButton.setOnClickListener {
+                if(ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this@MainActivity, "이미 권한이 있습니다.", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }
+
+//            // 카메라 권한 요청 - 2
+//            perButton.setOnClickListener {
+//                permissionLauncher.launch(Manifest.permission.CAMERA)
+//            }
         }
     }
 
@@ -52,6 +72,17 @@ class MainActivity : AppCompatActivity() {
             val intent = it.data
             val returnValue = intent!!.getStringExtra("two")
             Toast.makeText(this, returnValue.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){  isGranted: Boolean ->
+        if(isGranted){
+            Toast.makeText(this, "허가", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
         }
     }
 }
