99d6467be380dca3aa524e9e49c6569ee235acff
super7615 <super7615@naver.com>
2022-04-15 23:48:16 +0900

    NavigationEx - 메인 Fragment 백버튼

--- NavigationEx/app/src/main/java/com/hanyeop/navigationex/MainActivity.kt
+++ NavigationEx/app/src/main/java/com/hanyeop/navigationex/MainActivity.kt
@@ -56,12 +56,16 @@ class MainActivity : AppCompatActivity() {
 
     var waitTime = 0L
     override fun onBackPressed() {
-        super.onBackPressed()
-        if(System.currentTimeMillis() - waitTime >=1500 ) {
-            waitTime = System.currentTimeMillis()
-            Toast.makeText(this,"뒤로가기 버튼을 한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show()
-        } else {
-            finish() // 액티비티 종료
+        if(navController.currentDestination?.id == R.id.bottomOneFragment) {
+            if (System.currentTimeMillis() - waitTime >= 1500) {
+                waitTime = System.currentTimeMillis()
+                Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                finish() // 액티비티 종료
+            }
+        }
+        else{
+            super.onBackPressed()
         }
     }
 }
