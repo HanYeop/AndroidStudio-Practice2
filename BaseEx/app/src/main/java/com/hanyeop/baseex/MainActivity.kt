63a47be910042386f5f471565922565da11d8945
super7615 <super7615@naver.com>
2022-04-25 17:28:39 +0900

    BaseEx - 중복코드 제거

--- BaseEx/app/src/main/java/com/hanyeop/baseex/MainActivity.kt
+++ BaseEx/app/src/main/java/com/hanyeop/baseex/MainActivity.kt
@@ -1,17 +1,8 @@
 package com.hanyeop.baseex
 
-import android.os.Bundle
 import com.hanyeop.baseex.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding.apply {
-
-        }
-    }
 
     override fun init() {
 
--- BaseEx/app/src/main/java/com/hanyeop/baseex/MainFragment.kt
+++ BaseEx/app/src/main/java/com/hanyeop/baseex/MainFragment.kt
@@ -1,10 +1,6 @@
 package com.hanyeop.baseex
 
-import android.os.Bundle
-import android.view.View
 import com.hanyeop.baseex.databinding.FragmentMainBinding
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -12,15 +8,10 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     private val _stateFlow = MutableStateFlow(200)
     val stateFlow = _stateFlow.asStateFlow()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun init() {
         binding.apply {
             main = this@MainFragment
         }
-    }
-
-    override fun init() {
-
     }
 
     fun changeStateFlow(){
