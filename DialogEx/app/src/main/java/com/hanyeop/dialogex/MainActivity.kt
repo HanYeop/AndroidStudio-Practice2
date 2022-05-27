b5ad1c155952b2ead564504deb177cbb0e69386f
super7615 <super7615@naver.com>
2022-05-27 23:11:01 +0900

    DialogEx - 1. 일반 다이얼로그 띄우기

--- DialogEx/app/src/main/java/com/hanyeop/dialogex/MainActivity.kt
+++ DialogEx/app/src/main/java/com/hanyeop/dialogex/MainActivity.kt
@@ -2,10 +2,43 @@ package com.hanyeop.dialogex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.hanyeop.dialogex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initClickListener()
+    }
+
+    private fun initClickListener(){
+        binding.apply {
+            btnDialogCommon.setOnClickListener {
+                showDialog()
+            }
+        }
+    }
+
+    // 1. 일반 다이얼로그 띄우기
+    private fun showDialog(){
+        val builder = AlertDialog.Builder(this)
+        builder
+            .setTitle("타이틀입니다.")
+            .setMessage("메세지입니다.")
+            .setPositiveButton("YES") { dialog , which ->
+                binding.dialogText.text = "YES Click"
+            }
+            .setNegativeButton("NO"){ dialog, which ->
+                binding.dialogText.text = "NO Click"
+            }
+            .create()
+            .show()
     }
 }
