2470f1631ef72e50940b71c7ae8c5b7de5a7e991
super7615 <super7615@naver.com>
2022-06-01 12:24:56 +0900

    NavigationEx2 - 상태 유지 (show/hide)

--- NavigationEx2/app/src/main/java/com/hanyeop/navigationex2/view/MainActivity.kt
+++ NavigationEx2/app/src/main/java/com/hanyeop/navigationex2/view/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.hanyeop.navigationex2.view
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.hanyeop.navigationex2.R
 import com.hanyeop.navigationex2.databinding.ActivityMainBinding
 
+private const val TAG = "테스트"
 class MainActivity : AppCompatActivity() {
-    // 네비게이션 선언
-    private lateinit var navController: NavController
 
     private lateinit var binding : ActivityMainBinding
+    private val fragmentManager = supportFragmentManager
+
+    private var oneFragment: OneFragment? = null
+    private var twoFragment: TwoFragment? = null
+    private var threeFragment: ThreeFragment? = null
+    private var floatingFragment = FloatingFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +30,60 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
 
-        initNavigation()
+        initBottomNavigation()
+
+        binding.floatingActionButton.setOnClickListener{
+            fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView,floatingFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
-    // 네비게이션 연결
-    private fun initNavigation(){
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController = navHostFragment.findNavController()
-        binding.bottomNavi.setupWithNavController(navController)
+    private fun initBottomNavigation(){
+        // 최초로 보이는 프래그먼트
+        oneFragment = OneFragment()
+        fragmentManager.beginTransaction().replace(R.id.fragmentContainerView,oneFragment!!).commit()
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.oneFragment -> {
-                    binding.textTitle.text = "화면 1"
+        binding.bottomNavi.setOnItemSelectedListener {
+
+            // 최초 선택 시 fragment add, 선택된 프래그먼트 show, 나머지 프래그먼트 hide
+            when(it.itemId){
+                R.id.oneFragment ->{
+                    if(oneFragment == null){
+                        oneFragment = OneFragment()
+                        fragmentManager.beginTransaction().add(R.id.fragmentContainerView,oneFragment!!).commit()
+                    }
+                    if(oneFragment != null) fragmentManager.beginTransaction().show(oneFragment!!).commit()
+                    if(twoFragment != null) fragmentManager.beginTransaction().hide(twoFragment!!).commit()
+                    if(threeFragment != null) fragmentManager.beginTransaction().hide(threeFragment!!).commit()
+
+                    return@setOnItemSelectedListener true
                 }
-                R.id.twoFragment -> {
-                    binding.textTitle.text = "화면 2"
+                R.id.twoFragment ->{
+                    if(twoFragment == null){
+                        twoFragment = TwoFragment()
+                        fragmentManager.beginTransaction().add(R.id.fragmentContainerView,twoFragment!!).commit()
+                    }
+                    if(oneFragment != null) fragmentManager.beginTransaction().hide(oneFragment!!).commit()
+                    if(twoFragment != null) fragmentManager.beginTransaction().show(twoFragment!!).commit()
+                    if(threeFragment != null) fragmentManager.beginTransaction().hide(threeFragment!!).commit()
+
+                    return@setOnItemSelectedListener true
                 }
-                R.id.threeFragment -> {
-                    binding.textTitle.text = "화면 3"
+                R.id.threeFragment ->{
+                    if(threeFragment == null){
+                        threeFragment = ThreeFragment()
+                        fragmentManager.beginTransaction().add(R.id.fragmentContainerView,threeFragment!!).commit()
+                    }
+                    if(oneFragment != null) fragmentManager.beginTransaction().hide(oneFragment!!).commit()
+                    if(twoFragment != null) fragmentManager.beginTransaction().hide(twoFragment!!).commit()
+                    if(threeFragment != null) fragmentManager.beginTransaction().show(threeFragment!!).commit()
+
+                    return@setOnItemSelectedListener true
+                }
+                else ->{
+                    return@setOnItemSelectedListener true
                 }
             }
         }
