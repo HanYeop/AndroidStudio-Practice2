3e66395c518a3afea9e2b137fcf50cb1cb54702c
super7615 <super7615@naver.com>
2022-07-07 10:16:21 +0900

    SplashEx - test

--- SplashEx/app/src/main/java/com/hanyeop/splashex/MainActivity.kt
+++ SplashEx/app/src/main/java/com/hanyeop/splashex/MainActivity.kt
@@ -2,10 +2,43 @@ package com.hanyeop.splashex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        installSplashScreen()
+
+        thread(start=true) {
+            for (i in 1..5) {
+                Thread.sleep(1000)
+            }
+            isReady = true
+        }
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (isReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
     }
 }
