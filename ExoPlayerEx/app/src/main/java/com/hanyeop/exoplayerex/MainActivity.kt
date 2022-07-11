45d6ad1035f6428a8f2864b471471e537984fbed
super7615 <super7615@naver.com>
2022-07-11 15:13:52 +0900

    ExoPlayerEx - 완료

--- ExoPlayerEx/app/src/main/java/com/hanyeop/exoplayerex/MainActivity.kt
+++ ExoPlayerEx/app/src/main/java/com/hanyeop/exoplayerex/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.hanyeop.exoplayerex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.hanyeop.exoplayerex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var simpleExoPlayer: SimpleExoPlayer? = null
+    private var videoUrl = "http://techslides.com/demos/sample-videos/small.mp4"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        initPlayer()
+    }
+
+    private fun initPlayer(){
+        simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
+        binding.playerView.player = simpleExoPlayer
+        buildMediaSource()?.let {
+            simpleExoPlayer?.prepare(it)
+        }
+    }
+
+    // 영상에 출력할 미디어 정보를 가져오는 클래스
+    private fun buildMediaSource(): MediaSource {
+        val dataSourceFactory = DefaultDataSourceFactory(this, "sample")
+        return ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(MediaItem.fromUri(videoUrl))
+    }
+
+    // 일시중지
+    override fun onResume() {
+        super.onResume()
+        simpleExoPlayer?.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        simpleExoPlayer?.stop()
+        simpleExoPlayer?.playWhenReady = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        simpleExoPlayer?.release()
     }
 }
