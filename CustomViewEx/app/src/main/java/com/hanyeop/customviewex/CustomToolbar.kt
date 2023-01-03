4192c0ed4e9eb86a44e45033d74ff7579cbb0e75
Han <super7615@naver.com>
2023-01-03 17:50:36 +0900

    CustomViewEx - 커스텀 툴바 dataBinding 통해 구현

--- CustomViewEx/app/src/main/java/com/hanyeop/customviewex/CustomToolbar.kt
+++ CustomViewEx/app/src/main/java/com/hanyeop/customviewex/CustomToolbar.kt
@@ -4,18 +4,16 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.TextView
 import com.google.android.material.appbar.AppBarLayout
+import com.hanyeop.customviewex.databinding.ToolbarDefaultBinding
 
 class CustomToolbar @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr : Int = 0
 ): AppBarLayout(context, attrs, defStyleAttr) {
 
+    private var binding = ToolbarDefaultBinding.inflate(LayoutInflater.from(context), this, true)
+
     init {
-        // TODO : binding
-        val view = LayoutInflater.from(context).inflate(R.layout.toolbar_default, this, false)
-        addView(view)
         attrs?.let { getAttrs(it) }
     }
 
@@ -26,25 +24,29 @@ class CustomToolbar @JvmOverloads constructor(
     }
 
     private fun setTypeArray(typedArray: TypedArray){
-        val backImage = typedArray.getResourceId(R.styleable.CustomToolbar_back_image, R.drawable.ic_arrow_back)
-        findViewById<ImageView>(R.id.iv_back_button).setImageResource(backImage)
+        binding.apply {
+            val backImage = typedArray.getResourceId(R.styleable.CustomToolbar_back_image, R.drawable.ic_arrow_back)
+            ivBackButton.setImageResource(backImage)
 
-        val title = typedArray.getString(R.styleable.CustomToolbar_title)
-        findViewById<TextView>(R.id.tv_title).text = title
+            val title = typedArray.getString(R.styleable.CustomToolbar_title)
+            tvTitle.text = title
+        }
 
+        // 데이터를 캐싱해두어 가비지컬렉션에서 제외시키도록 하는 함수
+        // typedArray 사용 후 호출해야하나, 커스텀뷰가 반복 사용되는 것이 아니라면 호출하지 않아도 무방하다.
         typedArray.recycle()
     }
 
     fun setBackImage(image: Int){
-        findViewById<ImageView>(R.id.iv_back_button).setImageResource(image)
+        binding.ivBackButton.setImageResource(image)
     }
 
     fun setTitle(title: String){
-        findViewById<TextView>(R.id.tv_title).text = title
+        binding.tvTitle.text = title
     }
 
     fun setBackButtonClickEvent(onClick: () -> Unit){
-        findViewById<ImageView>(R.id.iv_back_button).setOnClickListener {
+        binding.ivBackButton.setOnClickListener {
             onClick()
         }
     }
